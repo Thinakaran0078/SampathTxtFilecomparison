0745d2558b78dd5759183f52484ff134eee9793b
Thinakaran0078 <thinakaran0078@gmail.com>
2021-12-22 18:30:34 +0530

    added pass html logic - intial draft

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 class Textdiff:
 
     def own_method(self):
@@ -55,22 +54,50 @@ class Textdiff:
                 html_txt += "<td>{}</td>".format(i+1)
                 html_txt += """<td bgcolor="red">{}</td>""".format("FAIL")
                 html_txt += "</tr>"
+                print(fail_dict)
 
             else:
                 #print("[Pass]The data comparison from file1 against file2[with repetitions]: ")
                 print(pass_list)
+                print(len(pass_list))
+
+                html_txt1 += "<tr>"
+                html_txt1 += "<td>File1</td>"
+                html_txt1 += "<td>{}</td>".format(pass_list[1])
+                html_txt1 += "<td>{}</td>".format(pass_list[0])
+                html_txt1 += """<td rowspan="2" bgcolor="green">{}</td>""".format('PASS')
+                html_txt1 += "</tr>"
+
+                if len(pass_list) == 3:
+                    html_txt1 += "<tr>"
+                    html_txt1 += "<td>File2</td>"
+                    html_txt1 += "<td>{}</td>".format(pass_list[1])
+                    html_txt1 += "<td>{}</td>".format(pass_list[2])
+                    html_txt1 += """<td bgcolor="green">{}</td>""".format('PASS')
+                    html_txt1 += "</tr>"
+                else:
+                    for rows in pass_list:
+                        html_txt1 += "<tr>"
+                        html_txt1 += "<td>File2</td>"
+                        html_txt1 += "<td>{}</td>".format(pass_list[1])
+                        html_txt1 += "<td>{}</td>".format(pass_list[0])
+                        html_txt1 += """<td bgcolor="green">{}</td>""".format('PASS')
+                        html_txt1 += "</tr>"
+
                 pass_list = []
                 counter = 0
 
         html_txt += "</table>"
         html_txt += "</body></html>"
 
+        html_txt1 += "</table>"
+        html_txt1 += "</body></html>"
+
         with open('fail.html', 'w') as file:
             file.writelines(html_txt)
 
-        print("[Fail]The data comparison from file1 against file2[with repetitions]: ")
-        for k, v in fail_dict.items():
-            print(f"row : {k}, value: {v}")
+        with open('pass.html', 'w') as file:
+            file.writelines(html_txt1)
 
 
 obj_ref = Textdiff()
